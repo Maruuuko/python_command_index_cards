a25de77244add8b4e1516cb156aa2476778b7cae
Maruko <131890771+Maruuuko@users.noreply.github.com>
2023-05-01 13:55:25 +0200

    Add files via upload

---
+++ for().py
@@ -0,0 +1,25 @@
+#1.
+
+#for i in range(10): print(i)
+
+#2.
+
+#for i in range(10,20,2): print(i)
+
+#3.
+
+#for i in range(5):
+#    print(++i)
+
+
+#6
+#multiplication_table = int(input("Enter your multiplication number: "))
+
+#print("The given number:", multiplication_table)
+
+#for i in range(1, 11):
+#    print(multiplication_table, "x", i, "=", multiplication_table * i)
+
+
+
+
